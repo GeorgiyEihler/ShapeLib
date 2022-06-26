87ab863d2d3672576a0008ca54e63f4cfc7bb592
Георгий Эйхлер <Georgiy.Eihler@outlook.com>
2022-06-26 14:23:30 +0600

    Change to async

--- src/Api/TestApi/Program.cs
+++ src/Api/TestApi/Program.cs
@@ -15,16 +15,16 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("GetCategories/{id?}", (IEshopDataContext context, int? id) =>
-    context.Categories.Where(c => id == null || c.Id == id));
+app.MapGet("GetCategories/{id?}", async (IEshopDataContext context, int? id) => 
+    await context.Categories.Where(c => id == null || c.Id == id).ToListAsync());
 
-app.MapGet("GetProducts", (IEshopDataContext context) => 
+app.MapGet("GetProducts", async (IEshopDataContext context) => 
      {
          var result = from p in context.Products
                       from c in context.Categories.Include(x => x.IdProucts).Where(pc => pc.IdProucts.Contains(p)).DefaultIfEmpty()
                       select new { p.ProductName, c.CategoryName };
 
-         return result;
+         return await result.ToListAsync();
 
      });
 
